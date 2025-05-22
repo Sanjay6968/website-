d9ae8e9954f64b9f50dabd95090c176cb31afc60
Sanjay6968 <146060936+Sanjay6968@users.noreply.github.com>
2025-05-22 11:02:46 +0530

    Update config.js

--- .tina/config.js
+++ .tina/config.js
@@ -1,16 +1,22 @@
-module.exports = {
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
   branch: "main",
-  clientId: "98047762-ce0f-4653-bbbc-ae1fa20e4ff3",
+  clientId: "98047762-ce0f-4653-bbbc-ae1fa20e4ff3", // from TinaCloud
+  token: process.env.TINA_GITHUB_TOKEN, // from your .env.local file
+
   build: {
     outputFolder: "admin",
     publicFolder: "public",
   },
+
   media: {
     tina: {
       mediaRoot: "uploads",
       publicFolder: "public",
     },
   },
+
   schema: {
     collections: [
       {
@@ -24,4 +30,5 @@ module.exports = {
       },
     ],
   },
-};
+});
+
