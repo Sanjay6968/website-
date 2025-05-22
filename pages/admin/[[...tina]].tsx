6c7e31154571449e3a89f2ac39ba1e9c5016903d
Sanjay6968 <146060936+Sanjay6968@users.noreply.github.com>
2025-05-22 11:13:57 +0530

    Update [[...tina]].tsx

--- pages/admin/[[...tina]].tsx
+++ pages/admin/[[...tina]].tsx
@@ -1,3 +1,9 @@
-import { TinaAdmin } from 'tinacms';
+import dynamic from 'next/dynamic';
+
+// Dynamically import TinaAdmin with SSR disabled
+const TinaAdmin = dynamic(() =>
+  import('tinacms').then((mod) => mod.TinaAdmin),
+  { ssr: false }
+);
 
 export default TinaAdmin;
